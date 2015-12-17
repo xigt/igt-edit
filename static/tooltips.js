c7f885394c5ffcd4f54194f0c864fc42879ca122
Ryan Georgi <rgeorgi@gmail.com>
2015-12-16 23:28:31 -0600

    Greatly simplify setting tooltips, and also provide a way for tooltips to be hidden upon clicking them.

--- static/tooltips.js
+++ static/tooltips.js
@@ -2,46 +2,44 @@
  * Created by rgeorgi on 12/3/15.
  */
 
+function tt(selector, content, hideOnClick) {
+    if (typeof hideOnClick === 'undefined') {hideOnClick = false;}
+
+    $(selector).each(function (i, el) {
+       if (!$(el).hasClass('tooltip-f')) {
+           $(selector).tooltip({
+              content:content
+           });
+           if (hideOnClick) {
+               $(selector).click(function(){
+                   $(this).tooltip('hide');
+               });
+           }
+       }
+    });
+}
+
 function assign_tooltips() {
     /*  */
-    //$('#rating-green').tooltip({
-    //    content:"I am confident that this IGT is clean.",
-    //});
-    //
-    //$('#rating-yellow').tooltip({
-    //    content : "I am not confident that this IGT is clean.",
-    //});
-    //
-    //$('#rating-red').tooltip({
-    //    content : "I am confident that this IGT is unclean.",
-    //});
+    tt('#rating-green', "I am confident that this IGT is clean.", true);
+    tt('#rating-yellow', "I am not confident that this IGT is clean.", true);
+    tt('#rating-red', "I am confident that this IGT is unclean.", true);
 
-    $('#glm').tooltip({
-        content: "Does the gloss line have the same number of morphs as the language line?"
-    });
-    $('#glw').tooltip({
-        content: "Does the gloss line have the same number of whitespace-separated tokens as the language line?"
-    });
-    $('#tag').tooltip({
-        content: "Does the normalized tier have any lines other than L, G, T?"
-    });
-    $('#col').tooltip({
-       content: "Are the tokens of the Language and Gloss line arranged using whitespace such that every token of one line is contained by another?"
-    });
+    tt('#glm', "Does the gloss line have the same number of morphs as the language line?");
+    tt('#glw', "Does the gloss line have the same number of whitespace-separated tokens as the language line?");
+    tt('#tag', "Does the normalized tier have any lines other than L, G, T?");
+    tt('#col', "Are the tokens of the Language and Gloss line arranged using whitespace such that every token of one line is contained by another?");
 
     hoverIconTooltips();
 }
 
+function labelTooltips() {
+    tt('.labelspan-AC', "Author Citation");
+    tt('.labelspan-DB', "Double Column");
+}
+
 function hoverIconTooltips() {
-    $('.undo').tooltip({
-        content: "Restore the tier to its initial state."
-    });
-
-    $('.delete').tooltip({
-       content: "Mark this tier for deletion, or remove if empty. Click again to undelete."
-    });
-
-    $('.add').tooltip({
-        content: "Add a blank new line beneath this line."
-    });
+    tt('.undo', "Restore the tier to its initial state.");
+    tt('.delete', "Mark this tier for deletion, or remove if empty. Click again to undelete.");
+    tt('.add', "Add a blank new line beneath this line.");
 }
