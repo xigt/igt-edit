c92fe8794f43c628a90b5ca194c2177c4b94346f
Ryan Georgi <rgeorgi@gmail.com>
2016-02-16 09:28:36 -0600

    1) Remove the old sleipnir db init script. 2) Sort the xigt corpora instances by a more intelligent parsing of the integers in the id strings. 3) Fix a bug in the cleaning script if a cleaned tier doesn't already exist.

---
+++ manage_db.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+
+from sleipnir import dbi
+
+def prompt_corpora():
+
+    # Main loop
+    main_loop = True
+    while main_loop:
+        corpora = dbi.list_corpora()
+        print("Current corpora:")
+        corpus_map = {}
+        for i, corpus in enumerate(sorted(corpora, key=lambda x: x['name'])):
+            corpus_map[i] = corpus
+            print('{:>6} {} [{}]'.format('({})'.format(i), corpus['name'], corpus['id']))
+        resp = input("Enter a corpus number to delete, or q to quit: ")
+        if resp == 'q':
+            main_loop = False
+        elif resp.strip() and int(resp) in corpus_map:
+            print('This will delete the corpus "#{}" with name "{}" and id "{}."'.format(i, corpus_map[i]['name'], corpus_map[i]['id']))
+            del_conf = input("Are you sure? [y/n]")
+            if del_conf.lower() == 'y':
+                dbi.del_corpus(corpus_map[int(resp)]['id'])
+
+
+
+if __name__ == '__main__':
+    p = ArgumentParser()
+    args = p.parse_args()
+
+    prompt_corpora()
